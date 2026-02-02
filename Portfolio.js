09a980d9f397376d8c44a676d27f2809591a9909
M.Ajith <ajithelumalai59@gmail.com>
2026-02-02 20:36:56 +0530

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,7 @@
+document.getElementById("b1").onclick=function(){
+            document.getElementById("h1").innerHTML="I am";
+            document.getElementById("h2").innerHTML="Apollo College Student";
+            document.getElementById("h3").innerHTML="In Bsc cs Group";
+            document.getElementById("i1").style.display='none';
+            document.getElementById("hide").style.display='block';
+        }
